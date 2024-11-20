37b857f758cc5972526f399d7287e0c00ef15abc
Matt Smith <matthew.a.w.smith@gmail.com>
2024-11-20 01:58:45 +0000

    Add initial followers logic

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,7 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
-    if not os.path.exists('log'):
+    if not os.path.exists('logs'):
         os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
     file_handler.setFormatter(logging.Formatter(
